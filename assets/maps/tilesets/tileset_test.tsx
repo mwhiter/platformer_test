d9060d6655fd6869cab38c8e13d2eb6bf3e26881
Michael Whiter <mjwhiter@hotmail.com>
2014-12-16 01:38:20 -0500

    Fixed maps.'

---
+++ assets/maps/tilesets/tileset_test.tsx
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset_test" tilewidth="16" tileheight="16">
+ <image source="tileset_test.png" width="256" height="32"/>
+ <tile id="1">
+  <properties>
+   <property name="TileType" value="TILE_SOLID"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="TileType" value="TILE_ONE_WAY"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="TileType" value="TILE_LADDER"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="TileType" value="TILE_SLOPE"/>
+   <property name="floor_left" value="15"/>
+   <property name="floor_right" value="12"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="TileType" value="TILE_SLOPE"/>
+   <property name="floor_left" value="11"/>
+   <property name="floor_right" value="8"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="TileType" value="TILE_SLOPE"/>
+   <property name="floor_left" value="7"/>
+   <property name="floor_right" value="4"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="TileType" value="TILE_SLOPE"/>
+   <property name="floor_left" value="3"/>
+   <property name="floor_right" value="0"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="TileType" value="TILE_SLOPE"/>
+   <property name="floor_left" value="0"/>
+   <property name="floor_right" value="3"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="TileType" value="TILE_SLOPE"/>
+   <property name="floor_left" value="4"/>
+   <property name="floor_right" value="7"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="TileType" value="TILE_SLOPE"/>
+   <property name="floor_left" value="8"/>
+   <property name="floor_right" value="11"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="TileType" value="TILE_SLOPE"/>
+   <property name="floor_left" value="12"/>
+   <property name="floor_right" value="15"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="TileType" value="TILE_SLOPE"/>
+   <property name="floor_left" value="0"/>
+   <property name="floor_right" value="7"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="TileType" value="TILE_SLOPE"/>
+   <property name="floor_left" value="8"/>
+   <property name="floor_right" value="15"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="TileType" value="TILE_SLOPE"/>
+   <property name="floor_left" value="15"/>
+   <property name="floor_right" value="8"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="TileType" value="TILE_SLOPE"/>
+   <property name="floor_left" value="7"/>
+   <property name="floor_right" value="0"/>
+  </properties>
+ </tile>
+</tileset>
